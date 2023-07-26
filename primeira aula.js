1665b280baea351bce682f34c0a1c0e6144d8fae
DiegoZandonadi <127964924+DiegoZandonadi@users.noreply.github.com>
2023-07-26 13:50:27 -0300

    Add files via upload

---
+++ primeira aula.js
@@ -0,0 +1,45 @@
+
+//Minha idade
+console.log("17") 
+
+//Quanto tempo programei nas férias
+console.log("uma semana")
+
+//5 variáveis diferentes
+let nome = "volta as aulas"
+
+//adicionar 3.5 a minha idade
+console.log(17 + 3.5)
+
+//quanto tempo passou desde 1969
+console.log(2023 - 1969)
+
+//divisão de números
+console.log(240/65)
+
+//multiplicação de números
+console.log(0.2708 * 100)
+
+//concatenação de strings
+console.log('hello' + 'world')
+
+//concatenação de strings com espaço
+console.log('hello ' + 'world ')
+
+//propriedade length
+console.log('test'.length)
+
+//método toUpperCase
+console.log('sus'.toLocaleUpperCase())
+
+//removendo espaço em branco
+console.log(' gosto de jogar '.trim())
+
+//gerar um número aleatório
+console.log(100*Math.random())
+
+//encontrando o próximo maior
+console.log(Math.ceil(43.8))
+
+//verificar se é um número inteiro
+console.log(Number.isInteger(2017))
